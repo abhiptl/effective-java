4fd87a2672e9d27e2c80813e9a3c644bd23ed7d8
Abhishek Patel <abhishek.patel@sanelib.com>
2017-09-24 12:33:13 +0530

    Item26: Favor generic types

---
+++ src/com/abhishek/item26/App26.java
@@ -0,0 +1,18 @@
+package com.abhishek.item26;
+
+/**
+ * Created by Abhishek Patel on 9/24/2017.
+ *
+ * Item26: Favor generic types
+ */
+public class App26 {
+    public static void main(String[] args) {
+        ObjectBasedStack objectBasedStack = new ObjectBasedStack();
+        objectBasedStack.push("Object Based Stack");
+        System.out.println("Pop from Object Based:"+ objectBasedStack.pop());
+
+        GenericBasedStack<String> genericBasedStack = new GenericBasedStack<>();
+        genericBasedStack.push("Generic String");
+        System.out.println("Pop from Generic Based:"+ genericBasedStack.pop());
+    }
+}
