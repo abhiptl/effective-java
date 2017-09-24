d97b5bef094654e45bd73935eda7b1e04bd786ee
Abhishek Patel <abhishek.patel@sanelib.com>
2017-09-24 12:06:12 +0530

    Item25: Prefer lists to arrays

---
+++ src/com/abhishek/item25/App25.java
@@ -0,0 +1,12 @@
+package com.abhishek.item25;
+
+/**
+ * Created by Abhishek Patel on 9/24/2017.
+ *
+ * Item25: Prefer lists to arrays
+ */
+public class App25 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/abhishek/item25/CoVariantArray.java
@@ -0,0 +1,31 @@
+package com.abhishek.item25;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by AbhishekPatel on 9/24/2017.
+ * `
+ */
+public class CoVariantArray {
+
+    public static void main(String[] args) {
+        CoVariantArray coVariantArray = new CoVariantArray();
+        coVariantArray.coVariance();
+    }
+
+    void coVariance() {
+        Object[] arrayObject = new Long[2];
+        arrayObject[1] = "I am String and get into Long container, oops";
+    }
+
+    void inVariance() {
+        // InCompitable types
+        //List<Object> listObject = new ArrayList<Long>();
+    }
+
+    void legalUseOfUnboundedWidcardArray() {
+        List<?>[] unBounded = new List[2];
+    }
+}
+
