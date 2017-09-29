236e288e4c77e3f9e0dcbbc9d6926a91a00c70ed
Abhishek Patel <abhishek.patel@sanelib.com>
2017-09-29 12:27:12 +0530

    Item37: Use marker interfaces to define types

---
+++ src/com/abhishek/_05_enums_annotations/item37/App37.java
@@ -0,0 +1,12 @@
+package com.abhishek._05_enums_annotations.item37;
+
+/**
+ * Created by AbhishekPatel on 9/29/2017.
+ *
+ * Item37: Use marker interfaces to define types
+ */
+public class App37 {
+    public static void main(String[] args) {
+
+    }
+}
