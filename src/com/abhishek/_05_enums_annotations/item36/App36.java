50f25162af2526d109d4e5d71f68a0e713700b93
Abhishek Patel <abhishek.patel@sanelib.com>
2017-09-29 12:16:07 +0530

    Item36: Consistently use the Override annotation

---
+++ src/com/abhishek/_05_enums_annotations/item36/App36.java
@@ -0,0 +1,12 @@
+package com.abhishek._05_enums_annotations.item36;
+
+/**
+ * Created by AbhishekPatel on 9/29/2017.
+ *
+ * Item36: Consistently use the Override annotation
+ */
+public class App36 {
+    public static void main(String[] args) {
+
+    }
+}
