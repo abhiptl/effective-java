75bf0e11ca1c478466c89db3a9a7085d0420cc4c
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-03 17:09:35 +0530

    Update welcome message

--- src/com/abhishek/EffectiveJavaMain.java
+++ src/com/abhishek/EffectiveJavaMain.java
@@ -3,6 +3,6 @@ package com.abhishek;
 public class EffectiveJavaMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Effective Java Programming");
+        System.out.println("Welcome to Effective Java Programming !!");
     }
 }
