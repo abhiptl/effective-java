1dc6fefcff5485a6d24fa012eb96c0a146fef3a6
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-13 09:56:09 +0530

    Item 55: Optimize judiciously
    More computing sins are committed in the name of efficiency (without necessarily
    achieving it) than for any other single reason—including blind stupidity.
    —William A. Wulf [Wulf72]
    We should forget about small efficiencies, say about 97% of the time: premature
    optimization is the root of all evil.
    —Donald E. Knuth [Knuth74]
    We follow two rules in the matter of optimization:
    Rule 1. Don’t do it.
    Rule 2 (for experts only). Don’t do it yet—that is, not until you have a
    perfectly clear and unoptimized solution.
    —M. A. Jackson [Jackson75]

---
+++ src/com/abhishek/_07_general_programming/item55/App55.java
@@ -0,0 +1,11 @@
+package com.abhishek._07_general_programming.item55;
+
+/**
+ *
+ * Item 55: Optimize judiciously
+ */
+public class App55 {
+    public static void main(String[] args) {
+
+    }
+}
