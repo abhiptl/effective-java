2d1db9dcda83ff24f51e604e89e9275470255561
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-13 09:05:42 +0530

    Item 52: Refer to objects by their interfaces

---
+++ src/com/abhishek/_07_general_programming/item52/App52.java
@@ -0,0 +1,11 @@
+package com.abhishek._07_general_programming.item52;
+
+/**
+ *
+ * Item 52: Refer to objects by their interfaces
+ */
+public class App52 {
+    public static void main(String[] args) {
+
+    }
+}
