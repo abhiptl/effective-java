4f2657a2c4a50bcf65e2c66d9cca39bc1e409845
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-13 09:43:03 +0530

    Item 54: Use native methods judiciously

---
+++ src/com/abhishek/_07_general_programming/item54/App54.java
@@ -0,0 +1,11 @@
+package com.abhishek._07_general_programming.item54;
+
+/**
+ *
+ * Item 54: Use native methods judiciously
+ */
+public class App54 {
+    public static void main(String[] args) {
+
+    }
+}
