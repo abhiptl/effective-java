cd5ec468d524048ccfa6ee1383aac0b3701dfd76
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-13 10:10:02 +0530

    Item 56: Adhere to generally accepted naming conventions

---
+++ src/com/abhishek/_07_general_programming/item56/App56.java
@@ -0,0 +1,11 @@
+package com.abhishek._07_general_programming.item56;
+
+/**
+ *
+ * Item 56: Adhere to generally accepted naming conventions
+ */
+public class App56 {
+    public static void main(String[] args) {
+
+    }
+}
