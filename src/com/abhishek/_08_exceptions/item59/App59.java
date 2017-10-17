711832cbde737f4e9a7a1ba64ff9a8a72f35024e
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-17 20:27:18 +0530

    Item 59: Avoid unnecessary use of checked exceptions

---
+++ src/com/abhishek/_08_exceptions/item59/App59.java
@@ -0,0 +1,11 @@
+package com.abhishek._08_exceptions.item59;
+
+/**
+ *
+ * Item 59: Avoid unnecessary use of checked exceptions
+ */
+public class App59 {
+    public static void main(String[] args) {
+
+    }
+}
