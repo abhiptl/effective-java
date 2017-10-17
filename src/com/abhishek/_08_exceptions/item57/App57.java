5af3469101f1516989c6fbfe4fff3776d1c380cd
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-17 20:07:55 +0530

    Item 57: Use exceptions only for exceptional conditions

---
+++ src/com/abhishek/_08_exceptions/item57/App57.java
@@ -0,0 +1,11 @@
+package com.abhishek._08_exceptions.item57;
+
+/**
+ *
+ * Item 57: Use exceptions only for exceptional conditions
+ */
+public class App57 {
+    public static void main(String[] args) {
+
+    }
+}
