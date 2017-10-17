6a47fe6bba514b64de19b56e011fdc3c2fbcc015
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-17 20:34:49 +0530

    Item 60: Favor the use of standard exceptions

---
+++ src/com/abhishek/_08_exceptions/item60/App60.java
@@ -0,0 +1,11 @@
+package com.abhishek._08_exceptions.item60;
+
+/**
+ *
+ * Item 60: Favor the use of standard exceptions
+ */
+public class App60 {
+    public static void main(String[] args) {
+
+    }
+}
