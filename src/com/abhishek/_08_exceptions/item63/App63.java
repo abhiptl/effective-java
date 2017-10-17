9b177ee61f6dfd628fb50022e45cc3c372f0c56f
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-17 20:56:15 +0530

    Item 63: Include failure-capture information in detail messages

---
+++ src/com/abhishek/_08_exceptions/item63/App63.java
@@ -0,0 +1,11 @@
+package com.abhishek._08_exceptions.item63;
+
+/**
+ *
+ * Item 63: Include failure-capture information in detail messages
+ */
+public class App63 {
+    public static void main(String[] args) {
+
+    }
+}
