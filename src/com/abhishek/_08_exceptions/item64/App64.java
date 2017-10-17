5a7b14a30837d2231c9eebd869d5f82bd6684936
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-17 21:02:13 +0530

    Item 64: Strive for failure atomicity

---
+++ src/com/abhishek/_08_exceptions/item64/App64.java
@@ -0,0 +1,11 @@
+package com.abhishek._08_exceptions.item64;
+
+/**
+ *
+ * Item 64: Strive for failure atomicity
+ */
+public class App64 {
+    public static void main(String[] args) {
+
+    }
+}
