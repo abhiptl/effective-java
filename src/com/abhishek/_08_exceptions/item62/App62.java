0266d858f45c686d6a2bead80f4b407f403cc17f
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-17 20:51:21 +0530

    Item62: Document all exceptions thrown by each method

---
+++ src/com/abhishek/_08_exceptions/item62/App62.java
@@ -0,0 +1,21 @@
+package com.abhishek._08_exceptions.item62;
+
+
+/**
+ * Item62: Document all exceptions thrown by each method
+ */
+public class App62 {
+
+}
+
+class VictimClass {
+    /**
+     *
+     * @throws com.abhishek._08_exceptions.item61.HighLevelException This is checked exception to mention in document
+     *
+     * It can throw NullPointerException. This is unchecked exception without @throw.
+     */
+    public void throwLowLevel()  {
+
+    }
+}
