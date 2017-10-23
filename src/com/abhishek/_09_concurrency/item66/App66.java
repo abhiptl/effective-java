a68db0426e076357b4b34a85ac1297fb5afb7776
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-23 10:58:45 +0530

    Item 66: Synchronize access to shared mutable data

---
+++ src/com/abhishek/_09_concurrency/item66/App66.java
@@ -0,0 +1,11 @@
+package com.abhishek._09_concurrency.item66;
+
+/**
+ *
+ * Item 66: Synchronize access to shared mutable data
+ */
+public class App66 {
+    public static void main(String[] args) {
+
+    }
+}
