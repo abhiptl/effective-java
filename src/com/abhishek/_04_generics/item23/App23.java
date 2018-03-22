d805b35c15bb360a6328ae16738d0e2a109e3373
Abhishek Patel <abhishek.patel@sanelib.com>
2018-03-22 21:40:37 +0530

    Item23 generics intro

--- src/com/abhishek/_04_generics/item23/App23.java
+++ src/com/abhishek/_04_generics/item23/App23.java
@@ -1,7 +1,10 @@
 package com.abhishek._04_generics.item23;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by AbhishekPatel on 9/23/2017.
+ * Created by Abhishek Patel on 9/23/2017.
  *
  * Item23: Don’t use raw types in new code
  *
@@ -9,6 +12,21 @@ package com.abhishek._04_generics.item23;
  */
 public class App23 {
     public static void main(String[] args) {
+        List rawlist = new ArrayList();
+        rawlist.add("string");
+        rawlist.add(new String[]{"1","2"});
+
+        List<Object> genericobjectlist = new ArrayList<>();
+        genericobjectlist.add("string");
+        genericobjectlist.add(new String[]{"1","2"});
+        genericobjectlist.add(new ArrayList<>());
+
+
+        List<?> unbounded = new ArrayList<>();
+        unbounded.add(null);
+
+
+
 
     }
 }
